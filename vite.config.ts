8cc3d42b996c6ee14bb51976dff3b7ce7f14b130
Ben Crow <48108358+CrowBe@users.noreply.github.com>
2026-04-07 06:28:58 +1000

    Fix favicon asset serving for Vercel builds

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import { resolve } from 'path';
 export default defineConfig({
   plugins: [preact()],
   root: './src',
+  publicDir: '../public',
   build: {
     outDir: '../dist/client',
     emptyOutDir: true,
